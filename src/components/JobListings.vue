51b338468c1b538773542c4ab4131efd7c0fcc32
“Ali <tadayoni.ali.enayat@gmail.com>
2024-11-18 16:54:56 +0330

    Implement Router with Vue-Router, Implemented Home,Job and NotFound Views and routes

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 import jobData from "../jobs.json";
 import JobListing from "./JobListing.vue";
 import { defineProps } from "vue";
+import { RouterLink } from "vue-router";
 
 defineProps({
   limit: Number,
@@ -31,10 +32,10 @@ const jobs = ref(jobData);
     </div>
   </section>
   <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-    <a
-      href="/jobs"
+    <RouterLink
+      to="/jobs"
       class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-      >View All Jobs</a
+      >View All Jobs</RouterLink
     >
   </section>
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from "../components/Hero.vue";
+import HomeCards from "../components/HomeCards.vue";
+import JobListings from "../components/JobListings.vue";
+</script>
+
+<template>
+  <Hero title="Lets do it" subtitle="Test for subtitle" />
+  <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
+</template>
